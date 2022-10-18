ac41be9f4eba8999577f2781a1c4a3f584b0946e
dchillon <david.chillon-sanchez@capgemini.com>
2022-10-18 13:05:21 +0200

    eliminacion archivos sobrantes

--- src/main/java/com/example/milanuncios/repository/AnuncioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.milanuncios.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.milanuncios.model.Anuncio;
-
-public interface AnuncioRepository extends CrudRepository<Anuncio, Integer>{
-
-}
--- src/main/java/com/example/milanuncios/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.milanuncios.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.milanuncios.model.Usuario;
-
-public interface UsuarioRepository extends CrudRepository<Usuario, String>{
-
-}
