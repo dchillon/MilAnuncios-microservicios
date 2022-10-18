eeed4c664b0d11fd32475ba5fb8d6b1283bb9fc0
Concheso Cubillas <carlos.concheso-cubillas@capgemini.com>
2022-10-18 13:38:51 +0200

    usuarios con microcosas

---
+++ microusuarios/src/test/java/com/example/microusuarios/MicrousuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrousuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
